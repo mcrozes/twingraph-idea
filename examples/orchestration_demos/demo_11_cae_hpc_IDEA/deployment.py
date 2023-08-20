3c3017a5266683024220d81aad21d4c0fcf9c321
mcrozes <mcrozes@amazon.com>
2023-08-20 22:12:00 +0200

    work in progress

---
+++ examples/orchestration_demos/demo_11_cae_hpc_IDEA/deployment.py
@@ -0,0 +1,83 @@
+import twingraph.awsmodules.idea.hpc_jobs as idea_hpc
+import base64
+
+# Local tests for now, must be added to Celery
+
+# All Supported Parameters
+# https://docs.ide-on-aws.com/hpc-simulations/user-documentation/supported-ec2-parameters
+# https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/
+
+# Standard HPC job on the "normal" queue using ALI2
+job_standard = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(b'echo "Standard HPC job running on IDEA"'),
+    base_os="amazonlinux2",
+    job_name="TestJob",
+    queue="normal",
+    nodes_count=1,
+    instance_type="t3.2xlarge",
+)
+
+# Standard HPC job using CentOS and different instance type
+job_standard_centos = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(b'echo "Standard CentOS HPC job running on IDEA"'),
+    base_os="centos7",
+    job_name="centosjob",
+    queue="normal",
+    nodes_count=1,
+    instance_type="m5.large",
+)
+
+# Spot job with spot price set to auto (=max on-demand price)
+job_spot = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(b'echo "Spot Job"'),
+    job_name="spot",
+    spot_price="auto",
+    nodes_count=1,
+    instance_type="m5.large",
+)
+
+# Job with 50 GB / partition and extra 90GB for /scratch partition
+job_extra_storage = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(b"df -h"),
+    job_name="JobExtraStorage",
+    nodes_count=1,
+    instance_type="t3.2xlarge",
+    root_size=50,
+    scratch_size=90,
+)
+
+# Job running on c5n with EFA automatically installed
+job_efa = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(b"fi_info -p efa -t FI_EP_RDM"),
+    job_name="TestJob",
+    nodes_count=1,
+    instance_type="c5n.18xlarge",
+    efa_support=True,
+)
+
+
+job_with_dependency = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(b'echo "Job will be executed after Standard HPC Job'),
+    job_name="RunAfterStandard",
+    depend="afterok",
+    depend_job_ids=job_standard["job_id"],
+    nodes_count=1,
+    instance_type="t3.2xlarge",
+)
+
+job_mpi = idea_hpc.submit_hpc_job(
+    job_body=base64.b64encode(
+        b"/apps/openmpi/4.1.5/bin/mpirun --host $(cat $PBS_NODEFILE | sort | uniq) hostname"
+    ),
+    job_name="MPI",
+    nodes_count=3,
+    instance_type="t3.2xlarge",
+)
+
+print(job_standard)
+print(job_standard_centos)
+print(job_spot)
+print(job_extra_storage)
+print(job_efa)
+print(job_with_dependency)
+print(job_mpi)
--- twingraph/awsmodules/idea/hpc_jobs.py
+++ twingraph/awsmodules/idea/hpc_jobs.py
@@ -4,14 +4,24 @@ import base64
 import subprocess
 import pathlib
 import sys
+import os
+import json
 
-# //print statement must be replaced with logging()
 
-OPENPBS_JOB_BIN_FOLDER = "/opt/bin"
+OPENPBS_JOB_BIN_FOLDER = "/opt/pbs/bin"
 QSUB = f"{OPENPBS_JOB_BIN_FOLDER}/qsub"
 QSTAT = f"{OPENPBS_JOB_BIN_FOLDER}/qstat"
 QDEL = f"{OPENPBS_JOB_BIN_FOLDER}/qdel"
 QALTER = f"{OPENPBS_JOB_BIN_FOLDER}/qalter"
+
+
+def validate_openpbs_binary() -> bool:
+    # will verify if qstat/qdel/sub etc are available on the system
+    for executable in [QSUB, QSTAT, QDEL, QALTER]:
+        if not pathlib.Path(executable).is_file():
+            print(f"{executable} not found. Update OpenPBS bin location if needed.")
+            return False
+    return True
 
 
 def run_cmd(cmd, shell=True) -> dict:
@@ -26,14 +36,18 @@ def run_cmd(cmd, shell=True) -> dict:
 
 # https://docs.ide-on-aws.com/hpc-simulations/user-documentation/supported-ec2-parameters
 # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/
-# For this PoC, I'm not planning to add all EC2 parameters supported by SOCA/IDEA
 def submit_hpc_job(
     job_body: base64.b64encode,
     nodes_count: int,
+    depend: str = False,  # Job dependency, must be after, afterok, afternotok or afterany
+    depend_job_ids: str = False,  # Job ID(s) dependency when depend parameter is set. ex: afterok:2 -> Job will be executed if job id 2 completed successfully.
     base_os: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#base_os
     efa_support: bool = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#efa_support
     force_ri: bool = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#force_ri
-    fsx_lustre: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#fsx_lustre
+    fsx_lustre: [
+        bool,
+        str,
+    ] = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#fsx_lustre
     fsx_lustre_size: int = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#fsx_lustre_size
     fsx_lustre_deployment_type: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#fsx_lustre_deployment_type
     ht_support: bool = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#ht_support_1
@@ -45,21 +59,56 @@ def submit_hpc_job(
     job_stderr_location: str = False,  # Absolute Path to the stderr file. Created automatically if False
     keep_ebs: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#keep_ebs
     placement_group: bool = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#placement_group
-    queue: str = False,  # HPC Queue to use, will use default if not set
+    queue: str = False,  # HPC Queue to use, will use IDEA default if not set
     root_size: int = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#root_size
     scratch_iops: int = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#scratch_iops
     scratch_size: int = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#scratch_size
     security_groups: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#security_groups
     spot_allocation_count: int = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#spot_allocation_count
     spot_allocation_strategy: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#spot_allocation_strategy
-    spot_price: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#spot_price
+    spot_price: [
+        str,
+        int,
+    ] = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#spot_price
     subnet_id: str = False,  # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/#subnet_id
+    idea_endpoint: str = os.environ.get(
+        "TWINGRAPH_IDEA_ENDPOINT", False
+    ),  # IDEA URL in order to submit HPC job remotely via HTTP APIs. If False, we assume TwinGraph is installed locally on an IDEA cluster
+    idea_username: str = os.environ.get(
+        "TWINGRAPH_IDEA_USERNAME", False
+    ),  # IDEA Username in order to submit HPC job remotely via HTTP APIs. Required if idea_endpoint is set
+    idea_password: str = os.environ.get(
+        "TWINGRAPH_IDEA_PASSWORD", False
+    ),  # IDEA Password in order to submit HPC job remotely via HTTP APIs. Required if idea_endpoint is set
 ) -> dict:
+    _openpbs_raw_job_template_data = locals()
+
+    # Validate OpenPBS bin
+    if not validate_openpbs_binary():
+        sys.exit(1)
+
+    if idea_endpoint and (not idea_username and not idea_password):
+        print("IDEA Username/Password must be specified when IDEA endpoint is enabled.")
+        sys.exit(1)
+    if idea_username or idea_password and not idea_endpoint:
+        print("`idea_endpoint` must be specified")
+        sys.exit(1)
+
+    _allowed_job_dependency = ["after", "afterok", "afternotok", "afterany", False]
+    if depend not in _allowed_job_dependency:
+        print(f"'depend' must be in {_allowed_job_dependency}, detected {depend}")
+        sys.exit(1)
+
+    if (depend and not depend_job_ids) or (depend_job_ids and not depend):
+        print(
+            f"'depend_job_ids' and 'depend' must be specified together. Detected depend: {depend}, depend_job_id: {depend_job_ids}"
+        )
+        sys.exit(1)
+
     # Retrieve function parameters and add new as needed
     # Note: parameters not set will inherit the default queue value
     # https://awslabs.github.io/scale-out-computing-on-aws/web-interface/create-your-own-queue/#option1-i-want-to-use-the-same-settings-as-an-existing-queue
     _job_uuid = str(uuid.uuid4())
-    _openpbs_raw_job_template_data = locals()
     _openpbs_raw_job_template_data["job_body_decoded"] = base64.b64decode(
         _openpbs_raw_job_template_data["job_body"]
     ).decode("utf-8")
@@ -68,9 +117,13 @@ def submit_hpc_job(
     _current_working_directory = pathlib.Path.cwd()
     _job_script_name = f"{_current_working_directory}/{_job_uuid}.qsub"
     if not job_stdout_location:
-        job_stdout_location = f"{_current_working_directory}/{_job_uuid}.stdout"
+        _openpbs_raw_job_template_data[
+            "job_stdout_location"
+        ] = f"{_current_working_directory}/{_job_uuid}.stdout"
     if not job_stderr_location:
-        job_stderr_location = f"{_current_working_directory}/{_job_uuid}.stderr"
+        _openpbs_raw_job_template_data[
+            "job_stderr_location"
+        ] = f"{_current_working_directory}/{_job_uuid}.stderr"
 
     # Job Template
     _openpbs_raw_job_template = """
@@ -79,18 +132,44 @@ def submit_hpc_job(
 # Add any additional PBS parameters as needed
 ############################
 
+{% if data.queue %}
 #PBS -q {{ data.queue }}
-#PBS -n {{ data.job_name }}
+{% endif %}
+
+#PBS -N {{ data.job_name }}
 #PBS -e {{ data.job_stderr_location}}
 #PBS -o {{ data.job_stdout_location}}
 
+{% if data.depend %}
+#PBS -W depend={{ data.depend }}:{{data.depend_job_ids}}
+{% endif %}
 ############################
 # SOCA/IDEA Scheduler Parameters
 # https://awslabs.github.io/scale-out-computing-on-aws/tutorials/integration-ec2-job-parameters/
 ############################
 
+
 {% for key,value in data.items() %}
-  {% if key not in ("nodes_count", "queue", "job_name", "job_body_decoded", "job_body") and value %}
+  {% if key in ('base_os',
+  'efa_support',
+  'force_ri',
+  'fsx_lustre',
+  'fsx_lustre_size',
+  'fsx_lustre_deployment_type',
+  'ht_support',
+  'instance_ami',
+  'instance_profile',
+  'instance_type',
+  'keep_ebs',
+  'placement_group',
+  'root_size',
+  'scratch_iops',
+  'scratch_size',
+  'security_groups',
+  'spot_allocation_count',
+  'spot_allocation_strategy',
+  'spot_price',
+  'subnet_id') and value %}
 #PBS -l {{key}}={{ value }}  
   {% endif %}
 {% endfor %}
@@ -106,53 +185,55 @@ def submit_hpc_job(
         [s for s in _unformatted_job_script.strip().splitlines(True) if s.strip()]
     )
 
-    # Save job script
+    if not idea_endpoint:
+        # No IDEA Username/Password specified, assuming TwinGraph is installed on IDEA and have access to qsub binary
+        try:
+            with open(_job_script_name, "w") as text_file:
+                text_file.write(_sanitized_job_script)
+        except FileNotFoundError as err:
+            print(
+                f"Unable to create {_job_script_name}. You most likely don't have write permission to this location"
+            )
+            sys.exit(1)
+        except Exception as err:
+            print(f"Unable to create {_job_script_name} due to {err}")
+            sys.exit(1)
 
-    try:
-        with open(_job_script_name, "w") as text_file:
-            text_file.write(_sanitized_job_script)
-    except FileNotFoundError as err:
-        print(
-            f"Unable to create {_job_script_name}. You most likely don't have write permission to this location"
-        )
-        sys.exit(1)
-    except Exception as err:
-        print(f"Unable to create {_job_script_name} due to {err}")
-        sys.exit(1)
+        print(f"Successfully created {_job_script_name}")
 
-    print(f"Successfully created {_job_script_name}")
+        _submit_job = run_cmd(f"{QSUB} {depend if depend else ''} {_job_script_name}")
+        # _submit_job = run_cmd(f"ls -ltr")
 
-    # _submit_job = run_cmd(f"{QSUB} {job_script_name}")
-    _submit_job = run_cmd(f"ls -ltr")
-
-    if int(_submit_job["return_code"]) == 0:
-        _job_id = _submit_job["stdout"].rstrip().lstrip()
+        """
+        {'job_uuid': 'dda015c3-8768-4725-86fc-25a8e0e1e901', 'job_id': '0.ip-60-0-114-81', 'job_stdout': False, 'job_stderr': False, 'qsub': {'return_code': 0, 'stdout': '0.ip-60-0-114-81\n', 'stderr': ''}}
+        """
+        if int(_submit_job["return_code"]) == 0:
+            _job_id = _submit_job["stdout"].rstrip().lstrip()
+            print(f"Job {_job_id} submitted successfully.")
+        else:
+            print(f"Unable to submit job due to {_submit_job}")
+            _job_id = None
     else:
-        _job_id = None
-
+        pass
     return {
         "job_uuid": _job_uuid,
         "job_id": _job_id,
-        "job_stdout": job_stdout_location,
-        "job_stderr": job_stderr_location,
+        "job_stdout_location": _openpbs_raw_job_template_data["job_stdout_location"],
+        "job_stderr_location": _openpbs_raw_job_template_data["job_stderr_location"],
         "qsub": _submit_job,
     }
 
 
-job_command = b"""
-ls -ltr
-pwd
-[[ if $? -ne 0 ]]; then
-  echo "test if"
-fi
-"""
+def get_job_output(job_uuid: uuid.uuid4) -> str:
+    pass
 
-z = submit_hpc_job(
-    job_body=base64.b64encode(job_command),
-    job_name="testjob",
-    nodes_count=5,
-    queue="test",
-    scratch_size=50,
-)
 
-print(z)
+def get_job_status(job_id: int) -> dict:
+    # _get_job_info = run_cmd(f"{QSTAT} -f {job_script_name} -Fjson")
+    _get_job_info = run_cmd(f"ls -ltr")
+    """
+    if int(_get_job_info["return_code"]) == 0:
+        _job_id = _submit_job["stdout"].rstrip().lstrip()
+    else:
+        _job_id = None
+    """
